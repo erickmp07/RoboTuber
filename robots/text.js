73225574d71f8dce0dd702c9acf83f9f49d35c6e
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-10-07 13:22:31 -0300

    Sanitizing Wikipedia content

--- robots/text.js
+++ robots/text.js
@@ -4,15 +4,41 @@ const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 async function robot(content) {
     await featchContentFromWikipedia(content)
     sanitizeContent(content)
-    breakContentIntoSentences(content)
+    //breakContentIntoSentences(content)
 
     async function featchContentFromWikipedia(content) {
-        const algorithmiaAuthenticated = algorithmia[algorithmiaApiKey]
+        const algorithmiaAuthenticated = algorithmia.client(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
         const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
         const wikipediaContent = wikipediaResponse.get()
 
         content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        content.sourceContentSanitized = withoutDatesInParentheses
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+
+            const withoutBlankLinesAndMarkdown = allLines.filter(line => {
+                if (line.trim().length == 0 ||
+                    line.trim().startsWith('=')) {
+                    return false
+                }
+
+                return true
+            })
+
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+    }
+
+    function removeDatesInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
     }
 }
 
