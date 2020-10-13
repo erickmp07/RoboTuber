2d76f37deb2c98de325a05893840c075a5a75a44
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-10-13 13:48:58 -0300

    Fetching at Google to get images links

--- robots/image.js
+++ robots/image.js
@@ -1,8 +1,33 @@
+const google = require('googleapis').google
+
 const state = require('./state.js')
+const googleSearchCredentials = require('../credentials/google-search.json')
+
+const customSearch = google.customsearch('v1')
 
 async function robot() {
     const content = state.load()
 
+    const imagesArray = await fetchGoogleAndReturnImagesLinks('Michael Jackson')
+
+    console.dir(imagesArray, { depth: null })
+    process.exit(0)
+
+    async function fetchGoogleAndReturnImagesLinks(query) {
+        const response = await customSearch.cse.list({
+            auth: googleSearchCredentials.apiKey,
+            cx: googleSearchCredentials.searchEngineID,
+            q: query,
+            searchType: 'image',
+            num: 2
+        })
+
+        const imagesUrl = response.data.items.map(item => {
+            return item.link
+        })
+
+        return imagesUrl
+    }
 }
 
 module.exports = robot
