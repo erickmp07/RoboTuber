f6f45762afe39de6cb59eeb897c5e0342d9c1862
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-10-17 16:06:56 -0300

    Starting web server

--- robots/youtube.js
+++ robots/youtube.js
@@ -1,3 +1,5 @@
+const express = require('express')
+
 const state = require('./state.js')
 
 async function robot() {
@@ -8,13 +10,29 @@ async function robot() {
     //await uploadThumbnail()
 
     async function authenticateWithOAuth() {
-        await startWebServer()
-        await createOAuthClient()
-        await requestUserConsent()
-        await waitForGoogleCallback()
-        await requestGoogleForAccessTokens()
-        await setGlobalGoogleAuthentication()
-        await stopWebServer()
+        const webServer = await startWebServer()
+        //await createOAuthClient()
+        //await requestUserConsent()
+        //await waitForGoogleCallback()
+        //await requestGoogleForAccessTokens()
+        //await setGlobalGoogleAuthentication()
+        //await stopWebServer()
+
+        async function startWebServer() {
+            return new Promise((resolve, reject) => {
+                const port = 5000
+                const app = express()
+
+                const server = app.listen(port, () => {
+                    console.log(`> Listening on http://localhost:${port}`)
+
+                    resolve({
+                        app,
+                        server
+                    })
+                })
+            })
+        }
     }
 }
 
